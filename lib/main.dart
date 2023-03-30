2dbb8c20bf6c0de57af8403aeb21fb65590572fd
kagangullu <oguzkagangullu@gmail.com>
2023-03-30 20:57:53 +0300

    template theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  _initSystemUi();
   runApp(const MyApp());
+}
+
+void _initSystemUi() {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.dark,
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,6 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Biren Koçluk',
+      debugShowCheckedModeBanner: false,
       home: Biren(),
     );
   }
@@ -22,7 +37,16 @@ class Biren extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "BİREN KOÇLUK",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        foregroundColor: const Color(0xfffc6406),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
     );
   }
 }
