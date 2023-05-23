2e56fc3c69d3d9b81fabf0756d4462ebf63b79e1
kagangullu <oguzkagangullu@gmail.com>
2023-05-23 20:57:25 +0300

    one step

--- lib/features/admin/view/create_homework/create_homework_view.dart
+++ lib/features/admin/view/create_homework/create_homework_view.dart
@@ -1,14 +1,12 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'package:biren_kocluk/features/admin/view/create_homework/create_homework_view_2.dart';
-import 'package:biren_kocluk/features/admin/view/students/student_edit_view.dart';
+import 'dart:convert';
+import 'package:biren_kocluk/features/admin/view/admin_home_view.dart';
 import 'package:biren_kocluk/product/enum/firebase_collection_enum.dart';
 import 'package:biren_kocluk/product/init/theme/light_theme_colors.dart';
-import 'package:biren_kocluk/product/model/user_model.dart';
-import 'package:biren_kocluk/product/widget/button/next_action_button.dart';
+import 'package:biren_kocluk/product/widget/button/done_action_button.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:date_field/date_field.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
 
@@ -27,6 +25,13 @@ class _AddEventState extends State<AddEvent> {
   DocumentSnapshot? selectedUser;
   int? grade;
   int? jsonNumber;
+
+  String? selectedSubjectValue;
+  String? selectedSubjectText;
+  String? selectedTopicValue;
+
+  Map<String, dynamic> subjects = {};
+  Map<String, dynamic> topics = {};
 
   String? statusMessage;
 
@@ -61,6 +66,27 @@ class _AddEventState extends State<AddEvent> {
     }
   }
 
+  Future<void> loadLessons() async {
+    final topicJsonString = await DefaultAssetBundle.of(context)
+        .loadString('assets/jsons/study/lessons.json');
+    final subjectJsonString = await DefaultAssetBundle.of(context)
+        .loadString('assets/jsons/study/subjects.json');
+    final topicData = await json.decode(topicJsonString);
+    final subjectData = await json.decode(subjectJsonString);
+
+    setState(() {
+      subjects = subjectData[0]["subjects"];
+      topics = topicData[jsonNumber][grade.toString()]?[selectedSubjectValue] ??
+          {"": ""};
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadLessons();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -73,46 +99,9 @@ class _AddEventState extends State<AddEvent> {
             context.emptySizedHeightBoxLow3x,
             _selectUserDropdown(),
             context.emptySizedHeightBoxLow3x,
-            statusMessage != null
-                ? RichText(
-                    text: TextSpan(
-                        text: statusMessage,
-                        style: context.textTheme.titleSmall
-                            ?.copyWith(color: LightThemeColors.black),
-                        children: <TextSpan>[
-                          TextSpan(
-                            text: " Sınıf Atayın.",
-                            style: context.textTheme.titleSmall
-                                ?.copyWith(color: LightThemeColors.red),
-                            recognizer: TapGestureRecognizer()
-                              ..onTap = () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => StudentEditView(
-                                      userModel: UserModel(
-                                        grade: selectedUser!["grade"],
-                                        classText: selectedUser!["class"],
-                                        name: selectedUser!["name"],
-                                        mail: selectedUser!["mail"],
-                                        password: selectedUser!["password"],
-                                        createdTime:
-                                            selectedUser!["createdTime"],
-                                        isVerified: selectedUser!["isVerified"],
-                                        uid: selectedUser!["uid"],
-                                      ),
-                                    ),
-                                  ),
-                                );
-                                setState(() {
-                                  statusMessage = null;
-                                  selectedUserValue = null;
-                                });
-                              },
-                          )
-                        ]),
-                  )
-                : const SizedBox.shrink()
+            _selectSubjectDropdown(),
+            context.emptySizedHeightBoxLow3x,
+            _selectTopicDropdown(),
           ],
         ),
       ),
@@ -129,38 +118,33 @@ class _AddEventState extends State<AddEvent> {
           child: const Icon(Icons.close_rounded),
         ),
         actions: [
-          _nextActionButton(context),
+          DoneActionButton(
+            color: grade == null ||
+                    selectedUserValue == null ||
+                    selectedDate == null ||
+                    jsonNumber == null ||
+                    selectedSubjectText == null ||
+                    selectedSubjectValue == null ||
+                    selectedTopicValue == null
+                ? LightThemeColors.blazeOrange.withOpacity(.6)
+                : LightThemeColors.blazeOrange,
+            onTap: () {
+              if (grade == null ||
+                  selectedUserValue == null ||
+                  selectedDate == null ||
+                  jsonNumber == null ||
+                  selectedSubjectText == null ||
+                  selectedSubjectValue == null ||
+                  selectedTopicValue == null) {
+                return;
+              } else {
+                _addHomework();
+              }
+            },
+          ),
           context.emptySizedWidthBoxNormal,
         ],
       );
-
-  NextActionButton _nextActionButton(BuildContext context) {
-    return NextActionButton(
-      onTap: () {
-        if (grade == null ||
-            selectedUserValue == null ||
-            selectedDate == null ||
-            jsonNumber == null) {
-          return;
-        } else {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (contex) => CreateHomeworkView2(
-                studentGrade: grade!,
-                studentValue: selectedUserValue!,
-                selectedDate: selectedDate!,
-                jsonNumber: jsonNumber!,
-              ),
-            ),
-          );
-        }
-      },
-      color: grade != null && selectedUserValue != null && selectedDate != null
-          ? LightThemeColors.blazeOrange
-          : LightThemeColors.blazeOrange.withOpacity(.6),
-    );
-  }
 
   DateTimeFormField _dateFormField() {
     return DateTimeFormField(
@@ -205,12 +189,99 @@ class _AddEventState extends State<AddEvent> {
               setState(() {
                 selectedUserValue = value;
                 userLoad();
+                selectedSubjectText = null;
+                selectedSubjectValue = null;
+                selectedTopicValue = null;
               });
             },
             items: userItems,
           );
         }
       },
+    );
+  }
+
+  DropdownButtonFormField<String> _selectSubjectDropdown() {
+    return DropdownButtonFormField(
+      isExpanded: true,
+      value: selectedSubjectValue,
+      hint: const Text("Ders seçiniz"),
+      onChanged: (value) {
+        setState(() {
+          selectedSubjectValue = value;
+          switch (selectedSubjectValue) {
+            case "turkish":
+              selectedSubjectText = "Türkçe";
+              break;
+            case "math":
+              selectedSubjectText = "Matematik";
+              break;
+            case "science":
+              selectedSubjectText = "Fen Bilimleri";
+              break;
+            case "english":
+              selectedSubjectText = "İngilizce";
+              break;
+            case "regligion":
+              selectedSubjectText = "Din";
+              break;
+            case "social":
+              selectedSubjectText = "Sosyal";
+              break;
+            default:
+          }
+          loadLessons();
+          if (selectedTopicValue != null) {
+            selectedTopicValue = null;
+          }
+        });
+      },
+      items: subjects.entries.map<DropdownMenuItem<String>>((entry) {
+        return DropdownMenuItem<String>(
+          value: entry.key,
+          child: Text(entry.value),
+        );
+      }).toList(),
+    );
+  }
+
+  DropdownButtonFormField<String> _selectTopicDropdown() {
+    return DropdownButtonFormField(
+      isExpanded: true,
+      value: selectedTopicValue,
+      hint: const Text("Konu seçiniz"),
+      onChanged: (value) {
+        setState(() {
+          selectedTopicValue = value;
+        });
+      },
+      items: selectedSubjectValue == null
+          ? null
+          : topics.entries.map<DropdownMenuItem<String>>((entry) {
+              return DropdownMenuItem<String>(
+                value: entry.value,
+                child: Text(entry.value),
+              );
+            }).toList(),
+    );
+  }
+
+  void _addHomework() async {
+    await FirebaseCollections.students.reference
+        .doc(selectedUserValue)
+        .collection("homeworks")
+        .add({
+      "userId": selectedUserValue,
+      "subject": selectedSubjectText,
+      "topic": selectedTopicValue,
+      "date": Timestamp.fromDate(selectedDate!),
+    });
+    Navigator.pushAndRemoveUntil(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const AdminHomeView(),
+      ),
+      (route) => false,
     );
   }
 }
