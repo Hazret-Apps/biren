878632414288d35bfb973a4b088a2d20acb36e0c
kagangullu <oguzkagangullu@gmail.com>
2023-06-17 18:35:15 +0300

    change font

--- lib/product/init/theme/theme.dart
+++ lib/product/init/theme/theme.dart
@@ -1,4 +1,5 @@
 import 'package:biren_kocluk/product/init/theme/light_theme_colors.dart';
+import 'package:biren_kocluk/product/init/theme/typography.dart';
 import 'package:flutter/material.dart';
 import 'package:kartal/kartal.dart';
 
@@ -7,19 +8,7 @@ class LightTheme {
 
   LightTheme(BuildContext context) {
     theme = ThemeData(
-      colorScheme: ColorScheme(
-        brightness: Brightness.light,
-        primary: LightThemeColors.blazeOrange.withOpacity(.3),
-        onPrimary: LightThemeColors.white,
-        secondary: LightThemeColors.white,
-        onSecondary: LightThemeColors.white,
-        error: LightThemeColors.red,
-        onError: LightThemeColors.red,
-        background: LightThemeColors.scaffoldBackgroundColor,
-        onBackground: LightThemeColors.white,
-        surface: LightThemeColors.white,
-        onSurface: LightThemeColors.black,
-      ),
+      colorScheme: _colorSheme(),
       useMaterial3: true,
       primarySwatch: Colors.orange,
       primaryColor: LightThemeColors.white,
@@ -27,44 +16,82 @@ class LightTheme {
       focusColor: LightThemeColors.blazeOrange,
       shadowColor: LightThemeColors.blazeOrange,
       indicatorColor: LightThemeColors.blazeOrange,
-      inputDecorationTheme: InputDecorationTheme(
-        filled: true,
-        fillColor: LightThemeColors.snowbank,
-        border: OutlineInputBorder(
-          borderRadius: context.normalBorderRadius,
-          borderSide: BorderSide.none,
-        ),
-      ),
+      inputDecorationTheme: _inputDecoration(context),
       scaffoldBackgroundColor: LightThemeColors.scaffoldBackgroundColor,
       hoverColor: LightThemeColors.blazeOrange,
       appBarTheme: const AppBarTheme(
         elevation: 0,
-        // foregroundColor: LightThemeColors.blazeOrange,
-        backgroundColor: LightThemeColors.scaffoldBackgroundColor,
+        centerTitle: true,
         titleTextStyle: TextStyle(
-          fontWeight: FontWeight.bold,
+          fontFamily: "Poppins",
           fontSize: 20,
+          fontWeight: FontWeight.w600,
           color: LightThemeColors.black,
-          // color: LightThemeColors.blazeOrange,
+        ),
+        backgroundColor: LightThemeColors.scaffoldBackgroundColor,
+      ),
+      floatingActionButtonTheme: _floatingActionButtonTheme(),
+      textButtonTheme: _textButton(),
+      textTheme: _textTheme(),
+    );
+  }
+
+  FloatingActionButtonThemeData _floatingActionButtonTheme() {
+    return const FloatingActionButtonThemeData(
+      backgroundColor: LightThemeColors.blazeOrange,
+      foregroundColor: LightThemeColors.white,
+    );
+  }
+
+  TextTheme _textTheme() {
+    return TextTheme(
+      displayLarge: MyTypography.headline1,
+      displayMedium: MyTypography.headline2,
+      displaySmall: MyTypography.headline3,
+      headlineMedium: MyTypography.caption1,
+      bodyLarge: MyTypography.body1,
+      bodyMedium: MyTypography.body2,
+      titleMedium: MyTypography.subhead,
+      bodySmall: MyTypography.caption2,
+    );
+  }
+
+  TextButtonThemeData _textButton() {
+    return TextButtonThemeData(
+      style: ButtonStyle(
+        overlayColor: MaterialStateColor.resolveWith(
+          (states) {
+            return Colors.orange.shade100;
+          },
         ),
       ),
-      floatingActionButtonTheme: const FloatingActionButtonThemeData(
-        backgroundColor: LightThemeColors.blazeOrange,
-        foregroundColor: LightThemeColors.white,
+    );
+  }
+
+  InputDecorationTheme _inputDecoration(BuildContext context) {
+    return InputDecorationTheme(
+      filled: true,
+      fillColor: LightThemeColors.snowbank,
+      border: OutlineInputBorder(
+        borderRadius: context.normalBorderRadius,
+        borderSide: BorderSide.none,
       ),
-      textButtonTheme: TextButtonThemeData(
-        style: ButtonStyle(
-          overlayColor: MaterialStateColor.resolveWith(
-            (states) {
-              return Colors.orange.shade100;
-            },
-          ),
-        ),
-      ),
-      textTheme: TextTheme(
-        titleLarge: context.textTheme.titleLarge
-            ?.copyWith(fontWeight: FontWeight.w800, fontSize: 20),
-      ),
+    );
+  }
+
+  ColorScheme _colorSheme() {
+    return ColorScheme(
+      brightness: Brightness.light,
+      primary: LightThemeColors.blazeOrange.withOpacity(.3),
+      onPrimary: LightThemeColors.white,
+      secondary: LightThemeColors.white,
+      onSecondary: LightThemeColors.white,
+      error: LightThemeColors.red,
+      onError: LightThemeColors.red,
+      background: LightThemeColors.scaffoldBackgroundColor,
+      onBackground: LightThemeColors.white,
+      surface: LightThemeColors.white,
+      onSurface: LightThemeColors.black,
     );
   }
 }
---
+++ lib/product/init/theme/typography.dart
@@ -0,0 +1,54 @@
+import 'package:biren_kocluk/product/init/theme/light_theme_colors.dart';
+import 'package:flutter/material.dart';
+
+class MyTypography {
+  static const _fontFamily = "Poppins";
+  static TextStyle headline1 = const TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 28,
+    fontWeight: FontWeight.w400,
+    color: LightThemeColors.black,
+  );
+  static TextStyle headline2 = const TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+    color: LightThemeColors.black,
+  );
+  static TextStyle headline3 = const TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: LightThemeColors.black,
+  );
+  static TextStyle body1 = const TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 17,
+    fontWeight: FontWeight.w400,
+    color: LightThemeColors.black,
+  );
+  static TextStyle body2 = const TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 17,
+    fontWeight: FontWeight.w600,
+    color: LightThemeColors.black,
+  );
+  static TextStyle subhead = const TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: LightThemeColors.black,
+  );
+  static TextStyle caption1 = const TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: LightThemeColors.black,
+  );
+  static TextStyle caption2 = const TextStyle(
+    fontFamily: _fontFamily,
+    fontSize: 11,
+    fontWeight: FontWeight.w400,
+    color: LightThemeColors.black,
+  );
+}
