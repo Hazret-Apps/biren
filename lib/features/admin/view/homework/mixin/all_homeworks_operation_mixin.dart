720f9ac6122d033191c0a309fc804405350922d9
kagangullu <oguzkagangullu@gmail.com>
2023-06-20 16:24:32 +0300

    stream fix

--- lib/features/admin/view/homework/mixin/all_homeworks_operation_mixin.dart
+++ lib/features/admin/view/homework/mixin/all_homeworks_operation_mixin.dart
@@ -1,9 +1,14 @@
 import 'package:biren_kocluk/features/admin/view/homework/all_homework_view.dart';
 import 'package:biren_kocluk/product/enum/firebase_collection_enum.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 mixin AllHomeworksOperationMixin on State<AllHomeworks> {
-  final Stream<QuerySnapshot> stream =
-      FirebaseCollections.homeworks.reference.snapshots();
+  final Stream<QuerySnapshot> stream = FirebaseCollections.homeworks.reference
+      .where(
+        "assignedUserID",
+        isEqualTo: FirebaseAuth.instance.currentUser!.uid,
+      )
+      .snapshots();
 }
