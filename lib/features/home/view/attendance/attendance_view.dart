30c3570136f4cdf37c56fee5c59c0ab8e1fc02b6
kagangullu <oguzkagangullu@gmail.com>
2023-07-01 20:03:34 +0300

    attendance view

---
+++ lib/features/home/view/attendance/attendance_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AttendanceView extends StatefulWidget {
+  const AttendanceView({super.key});
+
+  @override
+  State<AttendanceView> createState() => _AttendanceViewState();
+}
+
+class _AttendanceViewState extends State<AttendanceView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Yoklama"),
+      ),
+    );
+  }
+}
